f162b9f45c75d57fac7ac6ec1cf2b3e6472e8d63
MitchellMatherly-Trojan <mitchell.matherly@trojans.dsu.edu>
2025-12-15 10:21:09 -0600

    commit 4

--- RR/mainpage.js
+++ RR/mainpage.js
@@ -25,4 +25,14 @@ async function loadTable() {
     });
 
 }
+function openNav() {
+    document.getElementById("mySidebar").style.width = "50%";
+    document.getElementById("main").style.marginLeft = "25%";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
 loadTable();
