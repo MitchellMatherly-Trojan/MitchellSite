fc14105033a452d8d74fe0fe9a537a72e35fde71
MitchellMatherly-Trojan <mitchell.matherly@trojans.dsu.edu>
2025-12-15 18:31:13 -0600

    commit 6

--- FAQ/main.js
+++ FAQ/main.js
@@ -8,4 +8,18 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
 }
 
+const faqItems = document.querySelectorAll('.itemTitle');
 
+faqItems.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+
+        if (answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+        } else {
+            document.querySelectorAll('.itemAnswer').forEach(ans => ans.style.maxHeight = null);
+
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        }
+    });
+});
